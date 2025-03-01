185e842de37c4a61b20913ec4b90aab9fa2f3c0d
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2025-03-01 15:42:26 +0800

    feat: add logout method API and setting the expiration of tokens

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
  */
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
+
 /*
  * Public Routes
  */
