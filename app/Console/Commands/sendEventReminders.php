80ae7de626a93c470679f6671cbfdbea307caa16
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2025-03-02 22:59:22 +0800

    feat: add custom artisan command to alerts users about events

---
+++ app/Console/Commands/sendEventReminders.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class sendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends notifications to all event attendees that event is starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        /**
+         *'attendees.user' : get the attendees with detail user relation
+         * [A, B] -> from A time to B time
+         */
+        $events = Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+
+        $eventCount = $events->count(); // counting data in collection
+        $eventLabel = Str::plural('event', $eventCount);
+
+        // show information on terminal
+        $this->info("Found $eventCount $eventLabel");
+
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $this->info("Notifying the user {$attendee->user->name}")
+            )
+        );
+
+        $this->info('Reminder notifications sent successfully!');
+    }
+}
